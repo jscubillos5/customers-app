d1d3b603cf80f25d697614a335edc9f98aa0474e
Juan Sebastián Cubillos Gonzalez <cubillos2093@hotmail.com>
2020-01-13 22:20:34 -0500

    CAPITULO DEL 7 AL 10 DEL CURSO ANGULAR & SPRING 5

---
+++ src/app/header/app.header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component(
+    {
+        selector: 'app-header',
+        templateUrl: './app.header.component.html'
+    })
+export class AppHeaderComponent {
+    title: string = 'App Angular';
+
+}
